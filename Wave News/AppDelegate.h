cf0316f65173e78c7c2c51c5fc249336cab35d22
Guyu Xue <Guyu Xue>
2017-05-10 18:54:47 -0700

    rss feed

---
+++ Wave News/AppDelegate.h
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.h
+//  Wave News
+//
+//  Created by Max Gu on 12/30/11.
+//  Copyright (c) 2011 guxinchi2000@gmail.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "OffLineMode.h"
+#import "Flurry.h"
+
+//@class ViewController;
+@class ZUUIRevealController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ZUUIRevealController *viewController;
+@property (nonatomic,retain) NSArray *newsList;
+@property (nonatomic, retain)     UILocalNotification *localNotif;
+
+@end
